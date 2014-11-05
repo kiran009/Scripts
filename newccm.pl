0108b250508da47ecfd65cdab5efa2b0859bca94
Kiran Kumar D <kiran.daadhi@evolving.com>
2014-11-05 20:22:04 +0530

    Changes added

--- newccm.pl
+++ newccm.pl
@@ -7,31 +7,37 @@ use File::Basename;
 #/************ Setting Environment Variables *******************/
 $ENV{'CCM_HOME'}="/opt/ccm71";
 $ENV{'PATH'}="$ENV{'CCM_HOME'}/bin:$ENV{'PATH'}";
-open(ccm_addr,"$ENV{'CCM_HOME'}/bin/ccm start -d /data/ccmdb/dsa -m -q -r build_mgr -h ccmuk1 -nogui |");
-$ENV{'CCM_ADDR'}=<ccm_addr>;
-close(ccm_addr);
 $CCM="$ENV{'CCM_HOME'}/bin/ccm";
-@platformList740=("JAVA5");
 $LINAS5_HOST=pedlinux5;
 $Scripts_Dir="/data/ccmbm/final_script/kiran_test";
 my @PatchFiles,@files,$patchnumber;
 my $patch_number;
+my @CRS,@crs,@tasks;
 $PatchReleaseVersion;
-$mailto='kiran.daadhi@evolving.com';
-#$mailto='kiran.daadhi@evolving.com Shreraam.Gurumoorthy@evolving.com Viswanath.Banakar@evolving.com Kumar.SK@evolving.com Sekhar.Sahu@evolving.com Anand.Gubbi@evolving.com';
+#$mailto='kiran.daadhi@evolving.com';
+$mailto='kiran.daadhi@evolving.com Shreraam.Gurumoorthy@evolving.com Viswanath.Banakar@evolving.com Kumar.SK@evolving.com Sekhar.Sahu@evolving.com Anand.Gubbi@evolving.com';
 #/* Global Environment Variables ******* /
 main();
 sub main()
 {
+	start_ccm();
 	fetch_readme();
 	read_readme();
-	#reconfigure_dev_proj_and_compile();
+	fetch_crs();
+	reconfigure_dev_proj_and_compile();
 	reconfigure_del_project();
 	find_binaries_tar();
 	send_email();
 	#move_cr_status();
 	ccm_stop();
 	exit;
+}
+
+sub start_ccm()
+{
+	open(ccm_addr,"$ENV{'CCM_HOME'}/bin/ccm start -d /data/ccmdb/dsa -m -q -r build_mgr -h ccmuk1 -nogui |");
+	$ENV{'CCM_ADDR'}=<ccm_addr>;
+	close(ccm_addr);
 }
 
 sub find_binaries_tar()
@@ -41,9 +47,9 @@ sub find_binaries_tar()
 	@files;
 	chomp(@PatchFiles);
 	$PatchReleaseVersion="4.0.0";
-	$PROV_LINAS5_DIR="DSA_FUR_Delivery-patch_linAS5_".$PatchReleaseVersion;
-	$dir=$PROV_LINAS5_DIR;
-	#$dir="/data/ccmbm/provident/DSA_FUR_Delivery-patch_linAS5_7.4.0";
+	$FUR_LINAS5_DIR="/data/ccmbm/dsa/DSA_FUR_Delivery-patch_linAS5_".$PatchReleaseVersion;
+	$dir=$FUR_LINAS5_DIR;
+	#$dir="/data/ccmbm/FURident/DSA_FUR_Delivery-patch_linAS5_7.4.0";
 	my $ftpdir="/u/kkdaadhi/Patch_$patch_number";
 	my %hash;
 	foreach (@PatchFiles)
@@ -53,11 +59,10 @@ sub find_binaries_tar()
         	$sourcedir=$destdir=$_;
 	        $sourcedir=~ s/\$FURHOME/$dir\/DSA_FUR_Delivery/g;
         	$destdir=~s/\$FURHOME/$ftpdir/g;
-	        print "$sourcedir and $destdir ";
+	        print "*************** $sourcedir and $destdir ***************\n";
 	        $hash{$sourcedir}=$destdir;
 	}
 	#Search the binaries in the delivery project
-	print "\@dirs is: @dirs and \@files is: @files and \@sourcedirs is: @sourcedirs \n";
 	foreach $dir(@dirs)
 	{
 	        $dir=~s/\$FURHOME/$ftpdir/g;
@@ -68,15 +73,33 @@ sub find_binaries_tar()
 	while(($key,$value)=each %hash) {
         	`cp -f $key $value`;
 	}
-	chdir $ftpdir;
-	`tar cvf Patch.tar \*`;
+	print "\$ftpdir is: $ftpdir \n";
+	#chdir $ftpdir;
+	chomp($patch_number);
+	`cd $ftpdir; rm -f Patch\_$patch_number\*;tar cvf Patch_$patch_number.tar bin; gzip -f \*\.tar`;		
 }
 
-
+sub fetch_crs()
+{
+	chdir $Scripts_Dir;
+	@CRS=`sed -n '/FIXES/,/AFFECTS/ p' $patch_number\_README.txt  | sed '\$ d' | grep -v 'FIXES' | sed '/^\$/d'`;
+	chomp(@CRS);
+	$PatchReleaseVersion="4.0.0";
+	$FUR_LINAS5_DIR="DSA_FUR_Delivery-patch_linAS5_".$PatchReleaseVersion;
+	$dir=$FUR_LINAS5_DIR;
+	#$dir="/data/ccmbm/FURident/DSA_FUR_Delivery-patch_linAS5_7.4.0";
+	my $ftpdir="/u/prathish/Jenkins/Patch_$patch_number";
+	my %hash;
+	foreach (@CRS)
+	{
+		my($cr,@temp)=split(/\s+/,$_);
+        	push(@crs,$cr);	               
+	}
+	print "\@crs are: @crs\n";	 
+}
 sub send_email()
 {
-$body="Patch is created and available at: /u/prathish/Patch_9996/Patch.tar";
-system("/usr/bin/mutt -a /tmp/gmake.log -a /tmp/reconfigure.log -s 'Build Notification' $mailto < $body ");
+system("/usr/bin/mutt -s 'FUR 4.0.0 PATCH BUILD COMPLETED and available at: /u/kkdaadhi/Patch_8749' -a /tmp/gmake.log -a /tmp/reconfigure_devproject.log -a /tmp/reconfigure_delproject.log $mailto < /dev/null ");
 }
 
 sub move_cr_status()
@@ -112,9 +135,18 @@ sub read_readme()
 		{
 			($temp,$TaskFullNumber)=split(/:/,$op);
             $TaskFullNumber=~ s/^\s+|\s+$//g; 
-			($temp,$TaskNumber)=split(/_/,$TaskFullNumber);
-			$TaskNumber=~ s/^\s+|\s+$//g;
-			($PatchNumber)=split(/,/,$TaskNumber);
+			$TaskFullNumber=~ s/^\s+|\s+$//g; 
+			@tasks=split(/,/,$TaskFullNumber);
+			foreach (@tasks)
+			{
+				my($temp,$task)=split(/_/,$_);
+				push(@tasknumbers,$task);
+				#($temp,$TaskNumber)=split(/_/,$TaskFullNumber);
+			}
+			print "***************\@tasknumbers are: @tasknumbers *************** \n";
+				#$TaskNumber=~ s/^\s+|\s+$//g;
+				#($PatchNumber)=split(/,/,$TaskNumber);
+				$PatchNumber=2345;
 		}
 		if($op =~ /AFFECTS/)
 		{
@@ -131,36 +163,21 @@ sub read_readme()
 }
 sub reconfigure_dev_proj_and_compile()
 {
-	# Reconfigure the project
-	#open OP, "<config.properties";
-	#	my @op=<OP>;
-	#	close OP	;
-	#	
-	#	foreach $op(@op)
-	#	{
-	#		if($op =~ /BUILD_PLTFORMS_740/)
-	#		{
-	#			my ($temp,$test)=split(/'/,$op);
-	#			($temp,$test1)=split(/'/,$test);
-	#	        @ary=split(/\s+/,$temp);
-	#			push(@ary,@platformList740);
-	#		}
-	#	}
 	$PatchReleaseVersion="4.0.0";
-	$PROV_LINAS5_DIR="DSA_FUR_Dev-patch_linAS5_".$PatchReleaseVersion;
-	$projectName=$PROV_LINAS5_DIR;
+	$FUR_LINAS5_DIR="DSA_FUR_Dev-patch_linAS5_".$PatchReleaseVersion;
+	$projectName=$FUR_LINAS5_DIR;
 	
 	# Set the CCM workarea 
 	$ccmworkarea=`$CCM wa -show -recurse $projectName`;
 	($temp,$workarea)=split(/'/,$ccmworkarea);
-	print "CCM WorkArea is: $workarea";
+	print "***************CCM WorkArea is: $workarea\n***************";
 
 	# Reconfigure the project
 	# DSA_FUR_Dev-patch_linAS5_7.4.0 9996
 	#$SYSTEM_FOLDER_NO_740=1252;
 	#`$CCM folder -modify -add_task $TaskNumber $SYSTEM_FOLDER_NO_740`;
-	`$CCM folder -modify -add_task $TaskNumber`;
-	`$CCM reconfigure -rs -r -p $projectName`;
+	`$CCM folder -modify -add_task @tasks 2>&1 1>/dev/null`;
+	`$CCM reconfigure -rs -r -p $projectName 2>&1 1>/tmp/reconfigure_devproject.log`;
 
 	# Go to pedlinux5 and gmake clean all
 	#`OST "cd $ccmworkarea; /usr/bin/gmake clean all;"`;
@@ -171,17 +188,12 @@ sub reconfigure_del_project()
 {
 	# Go to Delivery project and reconcile and build the tar file
 	$PatchReleaseVersion="4.0.0";
-	$PROV_LINAS5_DIR="DSA_FUR_Delivery-patch_linAS5_".$PatchReleaseVersion;
-	$projectName=$PROV_LINAS5_DIR;
+	$FUR_LINAS5_DIR="DSA_FUR_Delivery-patch_linAS5_".$PatchReleaseVersion;
+	$projectName=$FUR_LINAS5_DIR;
+	print "*************** Delivery projectName is: $projectName  ***************\n";
 	$ccmworkarea=`$CCM wa -show -recurse $projectName`;
 	($temp,$workarea)=split(/'/,$ccmworkarea);
+	print "***************CCM WorkArea of Delivery Project is: $workarea***************\n";
 	#`$CCM reconcile -missing_wa_file -update_wa $workarea 2>&1 1>/tmp/reconcile.log`;
-	`$CCM reconfigure -rs -r -p $projectName 2>&1 1>/tmp/reconfigure.log`;
-
-	#foreach (@PatchFiles)
-	#{
-	#        print "\$PatchFiles is: $_";
-        #	push(@files,basename($_));
-	#}
-	#print "Value of \@files is: @files and Value of \@PatchFiles is : @PatchFiles\n";
+	`$CCM reconfigure -rs -r -p $projectName 2>&1 1>/tmp/reconfigure_delproject.log`;
 }
